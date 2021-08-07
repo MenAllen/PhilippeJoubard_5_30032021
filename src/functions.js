7eec1c31a882966615c4100c05a43dfac7b035d2
Philippe Joubard <philippe.joubard@sfr.fr>
2021-08-07 11:33:09 +0200

    livraison fichiers produit

--- src/functions.js
+++ src/functions.js
@@ -11,31 +11,19 @@
 // * products: [string] <-- array of product _id
 
 class Product {
-    constructor(id, name, description, price, option, quantity, imgurl) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = +price;
-        this.option = option;
-        this.quantity = +quantity;
-        this.imgurl = imgurl;
-    }
+	constructor(id, name, description, price, option, quantity, imgurl) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = +price;
+		this.option = option;
+		this.quantity = +quantity;
+		this.imgurl = imgurl;
+	}
 }
 
 //Variables Globales
 const myBasket = JSON.parse(localStorage.getItem("furniture")) || [];
 console.log(myBasket);
-const myProduct = new Product ();
+const myProduct = new Product();
 console.log(myProduct);
-
-function myBasketContent() {
-    if (myBasket.length == 0) {
-    } else {
-        let addBasketPreview = document.getElementById("basketPreview");
-        let calculBasketPreview = 0;
-        for (product of basket) {
-            calculBasketPreview += product.quantity;
-        }
-        addBasketPreview.innerHTML = `Panier <span class="badge rounded-pill bg-secondary align-middle my-auto">${calculBasketPreview}</span>`;
-    }
-}
